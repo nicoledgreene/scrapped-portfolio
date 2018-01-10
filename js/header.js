48fd46eb163fbbac28f63cd0290074df32d7d804
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-09 17:09:21 -0800

    stashed pictures

--- js/header.js
+++ js/header.js
@@ -1,19 +1,22 @@
+/* global $ */
+'use strict';
+
 function createString(input){
-   $(input).each(function(){
-     let myStr = $(this).html();
-     myStr = myStr.split('');
+  $(input).each(function(){
+    let myStr = $(this).html();
+    myStr = myStr.split('');
      
-     let contents = '';
-     for (let i = 0, len = myStr.length; i < len; i) {
-       contents = '<span class="single-char char-'  i  '">'  myStr[i]  '</span>';
-     }
-     $(input).html(contents);
-   });
- }
+    let contents = '';
+    for (let i = 0, len = myStr.length; i < len; i) {
+      contents = '<span class="single-char char-' +  i +  '">' + myStr[i] +  '</span>';
+    }
+    $(input).html(contents);
+  });
+}
  
- $('document').ready(function() {
-   let myHeader = $('#main-header');
-   createString(myHeader);
-   $('.char-3').addClass('accent');
-   $('.char-11').addClass('accent');
- }); 
+$('document').ready(function() {
+  let myHeader = $('#main-header');
+  createString(myHeader);
+  $('.char-3').addClass('accent');
+  $('.char-11').addClass('accent');
+}); 
